942ad37640bf36f0a7a37b2e2aa24411347c17ed
Ilka Gilge <ilka.gilge@school4games.net>
2015-01-12 14:15:17 +0100

    Ingame Menu Go Monkey Go
    Unity Scene, Buttons, Panels für Menü

---
+++ Unity Menu Go Monkey Go/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour
+{
+	
+
+	// Load the Game Scene
+	public void StartGame () 
+	{
+		Application.LoadLevel ("Game");
+		}
+	// end the Game
+	public void EndGame()
+		{
+		Application.Quit ();
+		}
+
+}
+	
+
